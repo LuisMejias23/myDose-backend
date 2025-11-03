b53e71fa7cc1cfc51a263bebfcbf0b89c0943ce1
Lois <lois@mejias.com>
2025-11-02 23:25:21 -0400

    Migración de Nodemailer a Resend para envío de correos

--- src/utils/email.js
+++ src/utils/email.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendEmail(to, subject, html) {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'MyDose <noreply@mydose.app>',
+      from: 'MyDose <onboarding@resend.dev>',
       to,
       subject,
       html,
